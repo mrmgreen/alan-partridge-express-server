cf00fd2b95b1fe0861deb1d67dadd130cec71dd5
mrmgreen <m.r.green@hotmail.co.uk>
2018-07-25 13:31:27 +0100

    adding a controller

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const rootController = require('./controller');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', rootController);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
